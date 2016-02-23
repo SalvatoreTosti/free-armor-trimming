7084dae63d7e68df83723c66e5062bf08758e6c5
Salvatore Tosti <saltosti@gmail.com>
2016-02-22 19:41:56 -0800

    Reworking how backgrounds are handled for Widgets

--- src/paint.py
+++ src/paint.py
@@ -8,6 +8,7 @@ from kivy.graphics import Color, Ellipse, Rectangle
 from kivy.uix.button import Button
 from kivy.uix.slider import Slider
 from kivy.core.window import Window
+from kivy.graphics import InstructionGroup
 
 from clickLogger import ClickLogger
 from clickPlayer import ClickPlayer
@@ -17,33 +18,40 @@ class RootWidget(Widget):
     def __init__(self,**kwargs):
         super(RootWidget,self).__init__(**kwargs)
         self.size = Window.size
+        with self.canvas:
+            Color(0,0,0)
+            Rectangle(pos = self.pos,size = self.size)
+
         # self.painter_proprtion_h = 1
         # self.painter_proprtion_v = .7
         # self.slider_proprtion_h = 1
         # self.slider_proprtion_v = .7
 
-        painter = MyPaintWidget()
-        painter.size = Window.size
+        self.painter = MyPaintWidget(size_hint=(1,.7))
+        self.bind(on_size=self.painter.paint_background)
+        #self.painter.size = Window.size
+        self.painter.update(0)
+        #self.painter.bind(size=self.painter.paint_background())
 
-        #painter.update(0)
-        self.painter = BackgroundWrapper(painter,size_hint=(1,.7))
-        self.painter.widget.update(0)
+        #self.painter = BackgroundWrapper(painter,size_hint=(1,.7))
+        #self.painter.widget.update(0)
+
+        self.slider = SliderBG(min=0,max=10,value=0,size_hint=(1,.3)) #Slider(min=0,max=10,value=0,size_hint=(1,.3))
+        #self.slider.size = Window.size
+
+        #self.slider = BackgroundWrapper(slider,size_hint=(1,.15))
 
         # self.painter = MyPaintWidget(size_hint=(1,.7))
         # self.painter.update(0)
         # self.slider = MySliderWidget(min=0,max=10,value=0,size_hint=(1,.3))
-        # self.slider.bind(value=self.update_canvas)
+        self.slider.bind(value=self.update_canvas)
         self.layout = BoxLayout(orientation='vertical')
         self.layout.size = self.size
-        # self.layout.size = self.size
         self.layout.add_widget(self.painter)
+        self.layout.add_widget(self.slider)
 
-        self.layout.add_widget(Button(text="kk",size_hint=(1,.3)))
-        #print self.painter.width
-        #print self.painter.height
-        # self.layout.add_widget(self.slider)
-        #
         self.add_widget(self.layout)
+
 
     def get_layout(self):
         return self.layout
@@ -53,21 +61,52 @@ class RootWidget(Widget):
     def update_canvas(self,obj,value):
         self.painter.update(value)
 
+
     def get_painter(self):
         return self.painter
     def get_slider(self):
         return self.slider
 
+class SliderBG(Slider):
+    def __init__(self,**kwargs):
+        super(SliderBG,self).__init__(**kwargs)
+        with self.canvas.before:
+            Color(.5,.5,.2)
+            self.rect = Rectangle(size=self.size,pos=self.pos)
+        self.bind(pos=self.update_rect,size=self.update_rect)
+
+    def update_rect(self,instance,value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
 
 class MyPaintWidget(Widget):
+    def __init__(self,**kwargs):
+        super(MyPaintWidget,self).__init__(**kwargs)
+        with self.canvas.before:
+            Color(.1,.1,.1)
+            self.rect = Rectangle(size=self.size,pos=self.pos)
+        self.bind(pos=self.update_rect,size=self.update_rect)
+
+    def update_rect(self,instance,value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+
+    def paint_background(self):
+        with self.canvas:
+            Color(0,1,0)
+            Rectangle(pos = self.pos,size = self.size)
+
     def update(self,sliderTime):
         with self.canvas:
+            print self.size
+            print self.pos
+
             eventQueue = self.mockEventList()
             for event in eventQueue:
                 time = event[0]
                 x = event[1]
                 y = event[2]
-                print "time, sliderTime: "+str(time)+", "+str(sliderTime)
+                #print "time, sliderTime: "+str(time)+", "+str(sliderTime)
                 if(time == math.floor(sliderTime)):
                     Color(1,0,0)
                     d=30
@@ -86,36 +125,6 @@ class MyPaintWidget(Widget):
         for num in range(1,10):
             eventQueue.append([num,num,num])
         return eventQueue
-
-class BackgroundWrapper(Widget):
-    def __init__(self,widget,**kwargs):
-        super(BackgroundWrapper,self).__init__(**kwargs)
-        self.widget = widget
-        size_x = self.size_hint[0]
-        size_y = self.size_hint[1]
-        window_x = Window.size[0]
-        window_y = Window.size[1]
-
-        self.background = BackgroundWidget(
-            height=widget.height*size_y,
-            width=widget.width*size_x,
-            pos_x=window_x*(1-size_x),
-            pos_y=window_y*(1-size_y))
-
-        self.background.width = widget.width
-        self.background.height = widget.height
-        self.add_widget(self.background)
-        self.add_widget(self.widget)
-
-
-class BackgroundWidget(Widget):
-    def __init__(self,pos_x,pos_y,**kwargs):
-        super(BackgroundWidget,self).__init__(**kwargs)
-        with self.canvas:
-            Color(1, .5, .2, 1)  # set the colour to orange
-            self.rect = Rectangle(
-                size=(self.width,self.height),
-                pos=(pos_x,pos_y))
 
 class MyPaintApp(App):
     def build(self):
