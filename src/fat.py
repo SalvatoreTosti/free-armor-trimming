e06b20f2e9ee39ffa1322dbb08115cbcec91d720
Salvatore Tosti <saltosti@gmail.com>
2016-05-17 19:56:33 -0700

    Overhauled command-line interface
    The command-line interface now functions as a looping program which prompts the user for instructions.

--- src/fat.py
+++ src/fat.py
@@ -5,32 +5,61 @@ from clickPlayer import ClickPlayer
 import argparse
 import os
 
+class MODE:
+    EXIT,ASK,EDIT,RECORD_CLICK,PLAY_CLICK,RECORD_MOUSE,PLAY_MOUSE = range(1,8)
+
 class FAT(object):
     def __init__(self):
         pass
 
     def main(self):
-        parser = argparse.ArgumentParser(description='Command line interface for clickLogger and clickPlayer')
-        group = parser.add_mutually_exclusive_group(required = True)
-        group.add_argument("--log", type=str, help="activate mouse click logging mode.")
-        group.add_argument("--edit", type=str, help="edit mouse click file.")
-        group.add_argument("--play", type=str, help="active mouse click playback mode.")
-        args = parser.parse_args()
+        print "Free Armor Trimming v0.1 by Salvatore Tosti"
+        running = True
+        mode = MODE.ASK
+        while running:
+            if(mode == MODE.EXIT):
+                running = False #stop looping
+            if(mode == MODE.ASK):
+                mode = self.promptForCommand()
+            elif(mode == MODE.EDIT):
+                pass #not implemented yet
+            elif(mode == MODE.RECORD_CLICK):
+                self.launchClickLogger()
+                mode = MODE.ASK
+                pass
+            elif(mode == MODE.RECORD_MOUSE):
+                pass
+            elif(mode == MODE.PLAY_MOUSE):
+                pass
+            else:
+                return
 
-        if args.log:
-            self.initClickLogger(args.log)
-        elif args.play:
-            if not os.path.isfile(str(os.getcwd()+ "/"+args.play)):
-                print "Invalid file selected."
-                return
-            else:
-                self.initClickPlayer(args.play)
-        elif args.edit:
-            if not os.path.isfile(str(os.getcwd()+ "/"+args.play)):
-                print "Invalid file selected."
-                return
-            else:
-                print "edit mode"
+
+    def promptForCommand(self):
+        print ""
+        userInput = raw_input("Enter an execution mode: ").lower()
+        if(userInput == "exit"):
+            return MODE.EXIT
+        elif(userInput == "edit"):
+            return MODE.EDIT
+        elif(userInput == "record click"):
+            return MODE.RECORD_CLICK
+        elif(userInput == "play click"):
+            return MODE.PLAY_CLICK
+        elif(userInput == "record key"):
+            return MODE.RECORD_CLICK
+        elif(userInput == "play click"):
+            return MODE.PLAY_CLICK
+        else:
+            return MODE.ASK
+
+    def promptForFile(self):
+        userInput = raw_input("Enter a file name: ")
+        return userInput
+
+    def launchClickLogger(self):
+        filename = self.promptForFile()
+        self.initClickLogger(filename)
 
     def initClickLogger(self, args):
         clickLogger = ClickLogger(args)
